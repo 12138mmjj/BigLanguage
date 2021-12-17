95675b6f9bda043d29bb05c8d6ed8b3c043e5b6d
B20031525袁杨 <2456591101@qq.com>
2021-12-17 15:20:32 +0800

    步骤3、调用静态库中的strcpy函数

--- StrFuncsExec/StrFuncsExec/StrFuncsExec.cpp
+++ StrFuncsExec/StrFuncsExec/StrFuncsExec.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include"StrStaticLib.h"
 using namespace std;
 
 int main()
@@ -6,7 +7,10 @@ int main()
 	char* str1 = NULL;
 
 	//调用静态库中的strcpy函数，将字符串"Windows"复制到str1，并输出str1，然后释放str1内存
-
+	cout << StringFuncs::MyStringFuncs::strcpy("Windows");
+	if (str1 != NULL) {
+		free(str1);
+	}
 	//调用静态库中的strcpy函数，将 NULL 复制到str1，并输出str1，然后释放str1内存
 
 	//调用静态库中的strcpy函数，将 "" 复制到str1，并输出str1，然后释放str1内存
