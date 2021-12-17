4b2e0a5dbdebe8ebc1dd86f62205b57ebbf19580
B20031525袁杨 <2456591101@qq.com>
2021-12-17 14:48:30 +0800

    步骤1、补充实现函数strcpy

--- StrStaticLib/StrStaticLib/StrcpyLib.cpp
+++ StrStaticLib/StrStaticLib/StrcpyLib.cpp
@@ -12,8 +12,22 @@ namespace StringFuncs
 	char* MyStringFuncs::strcpy(const char* src)
 	{
 		//实现函数strcpy
+		int length = 0;
+		const char* p1 = src;
+		while (*p1++ != 0)
+		{
+			length++;
+		}
+		char* dest = (char*)malloc(length + 1);
+		for (int i = 0; i < length; i++)
+		{
+			dest[i] = src[i];
+		}
+		dest[length] = 0;
+		return dest;
+		//实现函数strcpy
 		//函数功能为复制字符串
-		return 0;
+	
 	}
 
 }
