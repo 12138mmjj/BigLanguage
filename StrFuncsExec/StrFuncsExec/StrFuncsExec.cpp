0676b14e69b87009e146ac32cb361c81abac6918
B20031525袁杨 <2456591101@qq.com>
2021-12-17 17:10:09 +0800

    步骤5、调用函数strcat

--- StrFuncsExec/StrFuncsExec/StrFuncsExec.cpp
+++ StrFuncsExec/StrFuncsExec/StrFuncsExec.cpp
@@ -7,8 +7,8 @@ int main()
 	char* str1 = NULL;
 
 	//调用静态库中的strcpy函数，将字符串"Windows"复制到str1，并输出str1，然后释放str1内存
-	str1= StringFuncs::MyStringFuncs::strcpy("Windows");
-	cout << str1;
+	str1 = StringFuncs::MyStringFuncs::strcpy("Windows");
+	cout << str1<<endl;
 	if (str1 != NULL) {
 		free(str1);
 	}
@@ -18,7 +18,11 @@ int main()
 
 
 	//调用静态库中的strcat函数，将字符串"Hello "和" World!" 拼接到str1，并输出str1，然后释放str1内存
-
+	str1=StringFuncs::MyStringFuncs::strcat("Hello ", " World!");
+	cout << str1<<endl;
+	if (str1 != NULL) {
+		free(str1);
+	}
 	//调用静态库中的strcat函数，将 NULL 和 NULL 拼接到str1，并输出str1，然后释放str1内存
 
 	//调用静态库中的strcat函数，将 NULL 和 "" 拼接到str1，并输出str1，然后释放str1内存
