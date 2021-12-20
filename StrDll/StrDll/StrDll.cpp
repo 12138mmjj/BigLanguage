245649818b9ba3c4698dc62ae7c34817f819bda6
B20031525袁杨 <2456591101@qq.com>
2021-12-20 17:24:21 +0800

    步骤7、实现strcmp函数

--- StrDll/StrDll/StrDll.cpp
+++ StrDll/StrDll/StrDll.cpp
@@ -5,8 +5,20 @@
 
 int MyStrcmp(const char* str1, const char* str2)
 {
-	//实现函数strcmp
-	//函数功能为字符串比较
-	
-	return 0;
+	int i = 0;
+	while (str1[i] != '\0' && str2[i] != '\0' && str1[i] == str2[i])
+	{
+		i++;
+	}
+	if (str1[i] == str2[i]) {
+		return 0;
+	}
+	else if (str1[i] > str2[i]){
+		return 1;
+	}
+	else if (str1[i] < str2[i]){
+		return -1;
+	}
 }
+
+
