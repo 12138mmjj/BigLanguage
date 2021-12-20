29a04da4dd52eb39965218772e81cb82a5f5d349
B20031525袁杨 <2456591101@qq.com>
2021-12-20 18:14:27 +0800

    步骤11、显式调用dll

--- StrFuncsExec3/StrFuncsExec3/StrFuncsExec3.cpp
+++ StrFuncsExec3/StrFuncsExec3/StrFuncsExec3.cpp
@@ -4,24 +4,44 @@
 #include "windows.h"
 
 using namespace std;
+typedef int (*PMyStrcmp)(const char* str1, const char* str2);
 
 int main()
 {
 	//使用LoadLibrary函数装载动态库
-
+	HMODULE hDll = LoadLibrary(L"StrDll.dll");
+	if (hDll == NULL){
+		return 0;
+    } 
 	//使用GetProcAddress获取动态库中的函数
+    PMyStrcmp MyStrcmp = (PMyStrcmp)GetProcAddress(hDll, "MyStrcmp");
 
 	//调用动态库中的MyStrcmp("Class","Classes")，期望输出结果 -1
-
+	if (MyStrcmp != NULL)
+	{
+		cout << MyStrcmp("Class", "Classes") << endl;
+	}
 	//调用动态库中的MyStrcmp("Class","Class")，期望输出结果 0
-
+	if (MyStrcmp != NULL)
+	{
+		cout << MyStrcmp("Class", "Class") << endl;
+	}
 	//调用动态库中的MyStrcmp("Class","C")，期望输出结果 1
-
+	if (MyStrcmp != NULL)
+	{
+		cout << MyStrcmp("Class", "C") << endl;
+	}
 	//调用动态库中的MyStrcmp("Class",NULL)，期望输出结果 1
-
+	if (MyStrcmp != NULL)
+	{
+		cout << MyStrcmp("Class", NULL) << endl;
+	}
 	//调用动态库中的MyStrcmp(NULL , NULL)，期望输出结果 0
-
+	if (MyStrcmp != NULL)
+	{
+		cout << MyStrcmp(NULL, NULL) << endl;
+	}
 	//使用FreeLibrary释放动态链接库
-
+	FreeLibrary(hDll);
 	return 0;
 }
