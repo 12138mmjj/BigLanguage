8e0e8a3bff7f03cee82d3c955184aa49326087b3
B20031525袁杨 <2456591101@qq.com>
2021-12-20 17:56:05 +0800

    步骤9、隐式调用dll中的函数

--- StrFuncsExec2/StrFuncsExec2/StrFuncsExec2.cpp
+++ StrFuncsExec2/StrFuncsExec2/StrFuncsExec2.cpp
@@ -1,19 +1,19 @@
 ﻿//本项目以隐式链接方式调用dll
 #include <iostream>
-
+#include"StrDll.h"
 using namespace std;
 
 int main()
 {
 	//调用动态库中的MyStrcmp("Class","Classes")，期望输出结果 -1
-
+	cout << MyStrcmp("Class", "Classes") << endl;
 	//调用动态库中的MyStrcmp("Class","Class")，期望输出结果 0
-
+	cout << MyStrcmp("Class", "Class") << endl;
 	//调用动态库中的MyStrcmp("Class","C")，期望输出结果 1
-
+	cout << MyStrcmp("Class", "C") << endl;
 	//调用动态库中的MyStrcmp("Class",NULL)，期望输出结果 1
-
+	cout << MyStrcmp("Class", NULL) << endl;
 	//调用动态库中的MyStrcmp(NULL , NULL)，期望输出结果 0
-
+	cout << MyStrcmp(NULL, NULL) << endl;
 	return 0;
 }
